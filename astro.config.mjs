aebc3bc69bd09d5800b9c4c4accbf3eb66987fc5
luis122448 <luis122448@gmail.com>
2023-07-29 22:26:49 -0500

    Mirror changes , for deployment in GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://luis122448.github.io',
-    base: '/animationland-astro/',
+    base: '/animationland-astro',
   });
